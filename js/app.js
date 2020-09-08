fb98ecfbbffbded9274fd4fddc6000007d8c519f
farahzuot <farahz997@eng.hu.edu.jo>
2020-09-08 09:41:17 +0300

    ed

--- js/app.js
+++ js/app.js
@@ -5,16 +5,16 @@ var username = prompt ('what is your name?');
 console.log ('username',username);
 alert('WELCOME! ' + username);
 
-var isownermale = prompt ('what do you think! am i a male ?').toLocaleLowerCase();
+var isownermale = prompt ('what do you think! am i a male ?');
 console.log ('isownerfemale',isownermale);
-if (isownermale === 'n') {
+if (isownermale.toLocaleLowerCase === ('n' || 'no') ) {
   alert('Correct!');
 } else {
   alert('haha not really');
 }
 var isownerteenager = prompt ('do you think that im older than eighteen ?').toLocaleLowerCase();
 console.log ('isownerteenager',isownerteenager);
-if (isownerteenager === 'y') {
+if (isownerteenager.toLocaleLowerCase === ('y' || 'yees')) {
   alert('are you one of my parents :D');
 } else {
   alert('nope guess it again');
@@ -22,7 +22,7 @@ if (isownerteenager === 'y') {
 
 var isownergamer = prompt ('what about knowing me closer! ummm am i a gamer ?').toLocaleLowerCase();
 console.log ('isownergamer',isownergamer);
-if (isownergamer === 'n') {
+if (isownergamer.toLocaleLowerCase === ('n' || 'no')) {
   alert('Yees right guessing');
 } else {
   alert('not really :( ');
@@ -30,7 +30,7 @@ if (isownergamer === 'n') {
 
 var isownernice = prompt ('do you think that im a nice person?').toLocaleLowerCase();
 console.log ('isownernice',isownernice);
-if (isownernice === 'y') {
+if (isownernice.toLocaleLowerCase === ('y' || 'yes')) {
   alert('hahaaha thanksss ' + username);
   document.write('<h3>a big show to the sweet visitor !</h3>');
 
